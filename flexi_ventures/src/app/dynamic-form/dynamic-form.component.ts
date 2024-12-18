a77454f3ed2ae42ac184548474523810415ac92d
charan kumar <charan.nandibaktula>
2024-12-18 20:38:33 +0530

    flexi-code-commit

--- flexi_ventures/src/app/dynamic-form/dynamic-form.component.ts
+++ flexi_ventures/src/app/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -8,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-form.component.css'
 })
 export class DynamicFormComponent {
+  public orderForm: FormGroup;
+  finalresult:any=''
+  disablefield:boolean=false
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      orderNo: ['', Validators.required],
+      orderedDate: ['', Validators.required],
+      orderedInfo: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(0)]],
+      refurbished: ['Yes'],
+      address: ['', Validators.required]
+    });
+  }
+    onSubmit(val: { value: any; })
+  {
+    this.finalresult=val?.value
+    debugger
+  }
+  enterorederNO(event: { key: any; },slval: { value: { orderNo: any; orderedDate: string; }; })
+  {
+    let val=slval?.value?.orderNo+event.key
+    debugger
+    if((+val<10 && (slval?.value?.orderedDate!="")) || (+val<20 && slval?.value?.orderedDate==''))
+    {
+      this.disablefield=true
+    }
+    else
+    {
+      this.disablefield=false
+    }
+  }
+
 
 }
